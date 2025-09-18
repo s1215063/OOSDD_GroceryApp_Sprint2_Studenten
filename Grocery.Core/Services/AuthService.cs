e1d0dea5b168bee65fff36437caf9e01d7629944
Menno <s1215063@student.windesheim.nl>
2025-09-18 20:45:39 +0200

    App.xaml.cs LoginViewModel beschikbaar gemaakt en MainPage geactiveerd, AppShell.xaml.cs route naar loginscherm geactiveerd

--- Grocery.Core/Services/AuthService.cs
+++ Grocery.Core/Services/AuthService.cs
@@ -13,9 +13,6 @@ namespace Grocery.Core.Services
         }
         public Client? Login(string email, string password)
         {
-            //Vraag de klantgegevens [Client] op die je zoekt met het opgegeven emailadres
-            //Als je een klant gevonden hebt controleer dan of het password matcht --> PasswordHelper.VerifyPassword(password, passwordFromClient)
-            //Als alles klopt dan klantgegveens teruggeven, anders null
             Client? client = _clientService.Get(email);
 
             if (client != null && PasswordHelper.VerifyPassword(password, client.Password))
